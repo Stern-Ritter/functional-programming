b90d94236d3f6957cff65fd466f978daa61e25df
Stern-Ritter <fadeew.a@mail.ru>
2022-01-19 23:18:03 +0300

    Refactor currying

--- src/currying/currying.ts
+++ src/currying/currying.ts
@@ -1,22 +1,22 @@
-interface CurriedFunction2<T, R> {
+type CurriedFunction2<T, R> = {
   (t1: T): (t2: T) => R;
   (t1: T, t2: T): R;
 }
 
-interface CurriedFunction3<T, R> {
+type CurriedFunction3<T, R> = {
   (t1: T): CurriedFunction2<T, R>;
   (t1: T, t2: T): (t3: T) => R;
   (t1: T, t2: T, t3: T): R;
 }
 
-interface CurriedFunction4<T, R> {
+type CurriedFunction4<T, R> = {
   (t1: T): CurriedFunction3<T, R>;
   (t1: T, t2: T): CurriedFunction2<T, R>;
   (t1: T, t2: T, t3: T): (t4: T) => R;
   (t1: T, t2: T, t3: T, t4: T): R;
 }
 
-interface CurriedFunction5<T, R> {
+type CurriedFunction5<T, R> = {
   (t1: T): CurriedFunction4<T, R>;
   (t1: T, t2: T): CurriedFunction3<T, R>;
   (t1: T, t2: T, t3: T): CurriedFunction2<T, R>;
@@ -24,8 +24,9 @@ interface CurriedFunction5<T, R> {
   (t1: T, t2: T, t3: T, t4: T, t5: T): R;
 }
 
+
 function currying<T, R>(
-  fn: (t1: T, t2: T, t3: T, t4: T, t5: T) => R
+  fn: (...args: [T, T, T, T, T]) => R
 ): CurriedFunction5<T, R> {
   return function inner(...args: T[]): any {
     if (fn.length !== args.length) {
@@ -36,13 +37,3 @@ function currying<T, R>(
   };
 }
 
-const func = (a: number, b: number, c: number, d: number, e: number): number =>
-  a + b + c + d + e;
-
-const hof = currying(func);
-
-console.log(hof(1, 2, 3, 4, 5)); // 15
-console.log(hof(2, 3, 4)(5, 6)); // 20
-console.log(hof(3, 4)(5, 6)(7)); // 25
-console.log(hof(4, 5)(6)(7, 8)); // 30
-console.log(hof(5)(6)(7)(8)(9)); // 35
